21448c90e975b29dfd8a9c1128846a91bb73a3a6
nmarchuk <nmarchuk@bu.edu>
2025-03-27 21:54:31 -0400

    maybe this is it?

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
-export default function(eleventyConfig) {
+module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("style_dark.css");
     eleventyConfig.addPassthroughCopy("style_light.css");
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("darkmode.js");
 
-    eleventyConfig.setFrontMatterParsingOptions({ excerpt: true })
+    eleventyConfig.setFrontMatterParsingOptions({ excerpt: true });
 };
