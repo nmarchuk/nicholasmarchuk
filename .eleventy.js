7c47c586100b1d55da5600810de5b03c7d8868dd
nmarchuk <nmarchuk@bu.edu>
2025-07-05 11:46:51 -0400

    setup rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+
 export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("style_dark.css");
     eleventyConfig.addPassthroughCopy("style_light.css");
@@ -5,4 +7,23 @@ export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("darkmode.js");
 
     eleventyConfig.setFrontMatterParsingOptions({ excerpt: true });
+
+    eleventyConfig.addPlugin(feedPlugin, {
+		type: "rss", // or "rss", "json", "atom"
+		outputPath: "/feed.xml",
+		collection: {
+			name: "post", // iterate over `collections.posts`
+			limit: 10,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "Nicholas Marchuk's Blog",
+			subtitle: "News and updates directly from Nick's mind.",
+			base: "https://nicholasmarchuk.com/",
+			author: {
+				name: "Nicholas Marchuk",
+				email: "info@nicholasmarchuk.com", // Optional
+			}
+		}
+	});
 }
