285af1edda01975ae1fbce181adfa2f1b34f40bd
nmarchuk <nmarchuk@bu.edu>
2026-05-12 20:09:23 -0400

    color picker

--- utils.js
+++ utils.js
@@ -1,24 +1,24 @@
-var countDownDate = new Date("Aug 22, 2025 0:00").getTime();
+// var countDownDate = new Date("Aug 22, 2025 0:00").getTime();
 
-var x = setInterval(function() {
+// var x = setInterval(function() {
 
-  var now = new Date().getTime();
+//   var now = new Date().getTime();
 
-  var distance = countDownDate - now;
+//   var distance = countDownDate - now;
 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("countdown").innerHTML = "🎉" + days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s 🎉";
+//   document.getElementById("countdown").innerHTML = "🎉" + days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s 🎉";
 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "THE BOOK IS OUT!";
-  }
-}, 1000);
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("countdown").innerHTML = "THE BOOK IS OUT!";
+//   }
+// }, 1000);
 
 document.addEventListener('DOMContentLoaded', function() {
     var lightbox = document.getElementById('lightbox');
